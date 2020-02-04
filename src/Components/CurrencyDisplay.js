c98a35359224512ef687d23a582c59060daf1dcb
Rico Hancock <richardl@hancock1.com>
2020-02-04 14:41:52 -0700

    Finished renderprop afternoon project

---
+++ src/Components/CurrencyDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CurrencyDisplay = (props) => {
+  return(
+    <p>US ${props.amount.toFixed(2)} - {props.currencyData.name}{' '}{props.currencyData.symbol}{(props.amount * props.currencyData.rate).toFixed(2)}</p>
+  )
+}
+
+export default CurrencyDisplay;
